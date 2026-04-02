0e446bdf1176c90c31c34f2a531b949a6a0999c3
SuitCostomize <developereactapp-fz>
2026-04-02 10:58:05 +0530

    force redeploy

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const allowedOrigins = [
   "http://localhost:3000",
   "https://primecaremedicaltransport-frontend.vercel.app",
   "https://primecaretransportations.com",
+  "https://www.primecaretransportations.com",
   "https://developereactapp-fz-primecaremedica-lemon.vercel.app"
 ];
 
